4d0771044c62ae4d99fbc086486b62bd1e1ffda4
fabito <fuechi@ciandt.com>
2016-09-02 01:29:25 -0300

    new compose file for dyndns

--- env.bash
+++ env.bash
@@ -3,6 +3,7 @@
 export HTPC_HOME="/tmp/htpc"
 export HTPC_USER_ID=$(id -u)
 export HTPC_GROUP_ID=$(id -u)
-export HTPC_TIMEZONE=$(</etc/timezone)
+export HTPC_TIMEZONEgs=$(</etc/timezone)
 export HTPC_RESTART_POLICY="no"
 export HTPC_DOMAIN="mydomain.com"
+export AFRAID_API_KEY="insert_afraid_hash"
