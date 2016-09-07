30c54e900f1be1754b598585019ed816c5ce0944
fabito <fuechi@ciandt.com>
2016-09-07 08:49:58 -0300

    switching to path instead of subdomain

--- env.bash
+++ env.bash
@@ -3,7 +3,8 @@
 export HTPC_HOME="/tmp/htpc"
 export HTPC_USER_ID=$(id -u)
 export HTPC_GROUP_ID=$(id -u)
-export HTPC_TIMEZONEgs=$(</etc/timezone)
+export HTPC_TIMEZONE=$(</etc/timezone)
 export HTPC_RESTART_POLICY="no"
-export HTPC_DOMAIN="mydomain.com"
+export HTPC_DOMAIN="toka"
 export AFRAID_API_KEY="insert_afraid_hash"
+export HTPC_DOMAIN_OWNER_EMAIL="owner@toka.org"
